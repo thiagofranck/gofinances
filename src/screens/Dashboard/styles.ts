c1e0676d894f4aaa8e7b64c2d4caa6c5a0371e93
Thiago Henrique <thiagohenrique776@gmail.com>
2021-06-22 15:32:46 -0300

    Instalação e configuração de fontes externas

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -8,6 +8,7 @@ export const Container = styled.View`
 `;
 
 export const Title = styled.Text`
+  font-family: ${({theme}) => theme.fonts.bold};
   color: ${({theme}) => theme.colors.title};
   font-size: 24px;
   font-weight: bold;
