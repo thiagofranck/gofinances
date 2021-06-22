4f2b7e88d4e34277a304c7427d58fd541202b10b
Thiago Henrique <thiagohenrique776@gmail.com>
2021-06-22 14:55:12 -0300

    Estilização global

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,14 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({theme}) => theme.colors.background};
+`;
 
+export const Title = styled.Text`
+  color: ${({theme}) => theme.colors.title};
+  font-size: 24px;
+  font-weight: bold;
 `;
