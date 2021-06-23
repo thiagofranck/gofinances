d49207969e69fb06b0af4fe0365d9f20b8bb5f70
Thiago Henrique <thiagohenrique776@gmail.com>
2021-06-23 18:24:41 -0300

    Listagem de transações dinâmicas

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -16,6 +16,7 @@ interface Category {
  }
 
 interface Data {
+  type: 'positive' | 'negative';
   title: string;
   amount: string;
   category: Category;
@@ -31,11 +32,14 @@ export function TransactionCard({ data }: Props) {
   return (
     <Container>
       <Title>{data.title}</Title>
-      <Amount>{data.amount}</Amount>
+      <Amount type={data.type}>
+        { data.type === 'negative' && '- ' }
+        { data.amount }
+      </Amount>
 
       <Footer>
         <Category>
-          <Icon name="dollar-sign"/>
+          <Icon name={data.category.icon}/>
           <CategoryName>{data.category.name}</CategoryName>
         </Category>
 
