047d794d784fbd8a50882a8488f968c6044b6094
Thiago Henrique <thiagohenrique776@gmail.com>
2021-06-24 18:34:19 -0300

    Input e botão adicionados

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'styled-components';
 import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 
 
 export default function App() {
@@ -21,7 +21,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register>
+
+      </Register>
     </ThemeProvider>
   )
 }
