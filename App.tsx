91cf5911adca5fb87262f92333c59967dfa0903d
Thiago Henrique <thiagohenrique776@gmail.com>
2021-06-30 13:19:50 -0300

    Instalação e configuração React Navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
+import { NavigationContainer } from '@react-navigation/native'
+
 import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
+import { Dashboard } from './src/screens/Dashboard';
 
 
 export default function App() {
@@ -21,7 +25,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
