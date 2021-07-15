549aa67e99e67ec8e7899fb89412a87a31aa4d71
Thiago Henrique <thiagohenrique776@gmail.com>
2021-07-14 23:59:27 -0300

    Finalização do filtro

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -30,7 +30,7 @@ interface formData {
 
 const schema = Yup.object().shape({
   name: Yup.string().required('Nome é obrigatório'),
-  amount: Yup.number().typeError('Informe um valor númerico').positive('O valor não pode ser negativo')
+  amount: Yup.number().typeError('Informe um valor númerico').positive('O valor não pode ser negativo').required('O valor é obrigatório'),
 });
 
 export function Register() {
